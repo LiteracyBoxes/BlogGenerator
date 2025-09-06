c8906023caa74e3a564509045f0d50e1138a6108
ken <litebo@literacyboxes.com>
2025-09-07 03:55:29 +0900

    Release 1.2.14

--- bloggenerator.php
+++ bloggenerator.php
@@ -6,7 +6,7 @@ Plugin URI: https://github.com/LiteracyBoxes/BlogGenerator
 GitHub Plugin URI: https://github.com/LiteracyBoxes/BlogGenerator
 GitHub Branch: main
 Description: ブログ用のカスタム関数をまとめたプラグイン
-Version: 1.2.13
+Version: 1.2.14
 Author: ken
 --- ChangeLog ---
 - テスト更新
